77b856eb3f422fc908afb878cae629404b1ac5ff
Illia Ruban <ilyruban@gmail.com>
2026-03-23 01:47:04 +0100

    Removed unnecessary comparisons

--- basic_operations.py
+++ basic_operations.py
@@ -4,7 +4,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-#download image 1
+#download image
 img = cv2.imread('./dataset/1.bmp', 0)
 img = cv2.bitwise_not(img)
 kernel = np.ones((5, 5), np.uint8)
@@ -45,74 +45,6 @@ plt.title("Після розкриття")
 plt.axis('off')
 
 plt.subplot(2, 3, 5)
-plt.imshow(img_closing, cmap='gray')
-plt.title("Після закриття")
-plt.axis('off')
-
-plt.tight_layout()
-plt.show()
-
-
-# --- Erosion ---
-plt.figure(figsize=(8, 4))
-
-plt.subplot(1, 2, 1)
-plt.imshow(img, cmap='gray')
-plt.title("Оригінальне зображення")
-plt.axis('off')
-
-plt.subplot(1, 2, 2)
-plt.imshow(img_erosion, cmap='gray')
-plt.title("Після звуження")
-plt.axis('off')
-
-plt.tight_layout()
-plt.show()
-
-
-# --- Dilation ---
-plt.figure(figsize=(8, 4))
-
-plt.subplot(1, 2, 1)
-plt.imshow(img, cmap='gray')
-plt.title("Оригінальне зображення")
-plt.axis('off')
-
-plt.subplot(1, 2, 2)
-plt.imshow(img_dilation, cmap='gray')
-plt.title("Після розширення")
-plt.axis('off')
-
-plt.tight_layout()
-plt.show()
-
-
-# --- Opening ---
-plt.figure(figsize=(8, 4))
-
-plt.subplot(1, 2, 1)
-plt.imshow(img, cmap='gray')
-plt.title("Оригінальне зображення")
-plt.axis('off')
-
-plt.subplot(1, 2, 2)
-plt.imshow(img_opening, cmap='gray')
-plt.title("Після розкриття")
-plt.axis('off')
-
-plt.tight_layout()
-plt.show()
-
-
-# --- Closing ---
-plt.figure(figsize=(8, 4))
-
-plt.subplot(1, 2, 1)
-plt.imshow(img, cmap='gray')
-plt.title("Оригінальне зображення")
-plt.axis('off')
-
-plt.subplot(1, 2, 2)
 plt.imshow(img_closing, cmap='gray')
 plt.title("Після закриття")
 plt.axis('off')
